a3676f7d1aae37ea94a088047ee630bad4e25a41
aphezbtw <kosarnevernut@gmail.com>
2026-01-20 15:34:43 +0400

    Update app.py

--- app.py
+++ app.py
@@ -293,4 +293,5 @@ if __name__ == '__main__':
     with app.app_context():
         init_database()
 
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
